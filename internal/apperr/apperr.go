e978a0c5ff64ccc3d9ff94ebd50b4087cbd28f5a
Chantouch Narasetaphan <inecrft4747@gmail.com>
2026-06-01 11:45:11 +0700

    Create app errors

---
+++ internal/apperr/apperr.go
@@ -0,0 +1,23 @@
+package apperr
+
+import "net/http"
+
+type Error struct {
+	HTTPStatus int
+	Message    string
+}
+
+func (e *Error) Error() string {
+	return e.Message
+}
+
+var (
+	ErrTicketNotFound       = &Error{http.StatusNotFound, "ticket not found"}
+	ErrTemplateNotFound     = &Error{http.StatusNotFound, "experiment template not found on this ticket"}
+	ErrDuplicateTemplate    = &Error{http.StatusConflict, "experiment template already added to this ticket"}
+	ErrTicketAlreadyClosed  = &Error{http.StatusConflict, "ticket is already closed"}
+	ErrTicketNotOpen        = &Error{http.StatusConflict, "cannot modify a closed ticket"}
+	ErrInvalidTransition    = &Error{http.StatusUnprocessableEntity, "status transition is not allowed"}
+	ErrClosedReasonRequired = &Error{http.StatusUnprocessableEntity, "closed_reason is required when closing a ticket manually"}
+	ErrInvalidStatus        = &Error{http.StatusBadRequest, "invalid ticket status value"}
+)
